6c5dc3e277bd66da76760c3a1807a906800bbc45
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2022-06-04 14:02:27 -0400

    Search engine added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
+    <div className="container">
       <header className="App-header">
-        <p>
-          Dictionary
-        </p>
+        <main>
+          <h1>Dictionary</h1>
+          <Dictionary />
+        </main>
       </header>
+    </div>
     </div>
   );
 }
