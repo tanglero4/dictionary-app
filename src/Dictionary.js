aa69fcc434215625ba0e9ff661b3817e0445edb3
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2022-06-04 14:28:40 -0400

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
+
 
 export default function Dictionary(){
     let [keyword, setKeyword]= useState("");
 
+    function handleResponse(response){
+        console.log(response.data[0]);
+    }
+
     function search(event){
         event.preventDefault();   
-        alert(`searching for ${keyword}`);
+
+        //Documentation: https://dictionaryapi.dev/
+        let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event){
