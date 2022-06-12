f6b43ef918695519e935536a31d4180e70349e26
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2022-06-12 18:47:01 -0400

    Added image API

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ export default function App() {
         <main>
           <Dictionary defaultKeyword="Euphoria"/>
         </main>
-        <footer>Coded by <a href='https://aquamarine-lollipop-4f3091.netlify.app/'>Tiffany Anglero</a></footer>
+        <footer>Coded by <a href='https://aquamarine-lollipop-4f3091.netlify.app/' target="_blank" rel="noreferrer" >Tiffany Anglero</a>
+        <br/> 
+        Open-sourced on <a href="https://github.com/tanglero4/dictionary-app" target="_blank" rel="noreferrer" >GitHub</a>
+        <br/>
+        Hosted on<a href="https://app.netlify.com/teams/tanglero4/overview?_ga=2.21182589.733901245.1647549891-1475953262.1647549891&_gac=1.191157720.1647549891.Cj0KCQjwuMuRBhCJARIsAHXdnqPc0QqXir1NymhInXx1kN-iVFS4uALmZt_0o6obiss4ncz6WFUGq74aApjsEALw_wcB&_gl=1%2A1eghec1%2A_gcl_aw%2AR0NMLjE2NDc1NDk4OTEuQ2owS0NRand1TXVSQmhDSkFSSXNBSFhkbnFQYzBRcVhpcjFOeW1oSW5YeDFrTi1pVkZTNHVBTG1adF8wbzZvYmlzczRuY3o2V0ZVR3E3NGFBcGpzRUFMd193Y0I" target="_blank" rel="noreferrer" > Netlify</a>
+        </footer>
       </header>
     </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results.js";
+import Photos from "./Photos.js";
 import "./Dictionary.css";
 
 
@@ -8,10 +9,17 @@ export default function Dictionary(props){
     let [keyword, setKeyword]= useState(props.defaultKeyword);
     let [results, setResults]= useState(null);
     let [loaded, setLoaded]= useState(false);
+    let [photos, setPhotos]= useState("");
 
-    function handleResponse(response){
+    function handleDictionaryResponse(response){
         setResults(response.data[0]);
     }
+
+    function handlePexelsResponse(response){
+        console.log(response);
+        setPhotos(response.data.photos);
+    }
+
 function handleSubmit(event){
     event.preventDefault();
     search();
@@ -19,7 +27,12 @@ function handleSubmit(event){
     function search(event){
         //Documentation: https://dictionaryapi.dev/
         let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        const pexelsApiKey="563492ad6f9170000100000194a18ec8ad8c47469a298aba1995848b";
+        const pexelsUrlApi= `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        const headers = {Authorization: `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsUrlApi, {headers: headers}).then(handlePexelsResponse);
     }
 
 function load(){
@@ -43,6 +56,7 @@ if (loaded){
         </form>
         </section>
         <Results results={results}/>
+        <Photos photos={photos} />
         </div>
     );
 }else{
